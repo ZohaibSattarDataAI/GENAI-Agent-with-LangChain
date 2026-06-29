2aab02003f1cc6f42c16a5aa759e7dc577bbc5e7
Zohaib Sattar <zabizubi86@gmail.com>
2026-06-29 19:53:37 +0500

    some modifications in structured_output with typedict

--- Langchain_course_Module/Structured output/structured_output_typedic.py
+++ Langchain_course_Module/Structured output/structured_output_typedic.py
@@ -1,5 +1,6 @@
 from typing_extensions import TypedDict
 from langchain_ollama import ChatOllama
+import time
 
 
 # Define Output Schema
@@ -14,7 +15,7 @@ llm = ChatOllama(
     model="qwen2.5:1.5b",
     temperature=1.5
 )
-
+start = time.time()
 # Structured Output
 structured_llm = llm.with_structured_output(Person)
 
@@ -23,4 +24,11 @@ response = structured_llm.invoke(
     "My name is Ali. I am 22 years old and I live in Lahore."
 )
 
+end = time.time()
+
+print(f"Time taken: {end - start} seconds")
+
 print(response)
+print(response.name)
+print(response.age)
+print(response.city)
