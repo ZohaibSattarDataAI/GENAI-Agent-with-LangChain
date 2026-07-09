6f76c2d23910a5d94b105c7442d1007feda3410e
Zohaib Sattar <zabizubi86@gmail.com>
2026-07-09 12:39:56 +0500

    modification file in text_loader

--- Langchain_course_Module/Document Loaders/text_loader.py
+++ Langchain_course_Module/Document Loaders/text_loader.py
@@ -1,27 +1,22 @@
-<<<<<<< HEAD
 from langchain_community.document_loaders import TextLoader
 
-loader = TextLoader(r"C:\Users\ZohaibSattar_Data_AI\Downloads\GENAI-Agent-with-LangChain\Langchain_course_Module\Document Loaders\LangChain_Document_Loaders_Code.txt")
+# Load first text file
+loader = TextLoader(
+    r"C:\Users\ZohaibSattar_Data_AI\Downloads\GENAI-Agent-with-LangChain\Langchain_course_Module\Document Loaders\LangChain_Document_Loaders_Code.txt"
+)
 
 documents = loader.load()
 
+print("===== File 1 =====")
 print(documents[0].page_content)
 
 
+# Load second text file
+loader1 = TextLoader(
+    r"C:\Users\ZohaibSattar_Data_AI\Downloads\GENAI-Agent-with-LangChain\Langchain_course_Module\Document Loaders\sample1.txt"
+)
 
-from langchain_community.document_loaders import TextLoader
+documents1 = loader1.load()
 
-loader = TextLoader(r"C:\Users\ZohaibSattar_Data_AI\Downloads\GENAI-Agent-with-LangChain\Langchain_course_Module\Document Loaders\sample1.txt")
-
-documents = loader.load()
-
-print(documents[0].page_content)
-=======
-from langchain_community.document_loaders import TextLoader
-
-loader = TextLoader("notes.txt")
-
-documents = loader.load()
-
-print(documents)
->>>>>>> 9a6bc73f6d5f929537512ac3ab725bce6f4ce2ed
+print("\n===== File 2 =====")
+print(documents1[0].page_content)
