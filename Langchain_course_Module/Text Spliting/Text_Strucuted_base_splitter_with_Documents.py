2e04daf82b2ff6aae02b8f41b2e30979616d5432
Zohaib Sattar <zabizubi86@gmail.com>
2026-07-12 00:23:38 +0500

    text struted with modification

--- Langchain_course_Module/Text Spliting/Text_Strucuted_base_splitter_with_Documents.py
+++ Langchain_course_Module/Text Spliting/Text_Strucuted_base_splitter_with_Documents.py
@@ -1,31 +1,29 @@
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import MarkdownHeaderTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+# Load PDF
 loader = PyPDFLoader(
     r"C:\Users\ZohaibSattar_Data_AI\Downloads\ZohaibSattar_Data_AI .pdf"
 )
 
 documents = loader.load()
 
-# Combine all pages into one text
-text = "\n".join([doc.page_content for doc in documents])
+print(f"Total Pages: {len(documents)}")
 
-headers_to_split_on = [
-    ("#", "Heading 1"),
-    ("##", "Heading 2"),
-    ("###", "Heading 3"),
-]
-
-splitter = MarkdownHeaderTextSplitter(
-    headers_to_split_on=headers_to_split_on
+# Recursive Text Splitter
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=500,
+    chunk_overlap=100
 )
 
-chunks = splitter.split_text(text)
+# Split documents
+chunks = text_splitter.split_documents(documents)
 
-print(f"Total Chunks: {len(chunks)}\n")
+print(f"\nTotal Chunks: {len(chunks)}\n")
 
 for i, chunk in enumerate(chunks, start=1):
-    print(f"Chunk {i}")
+    print(f"========== Chunk {i} ==========")
     print(chunk.page_content)
-    print(chunk.metadata)
+    print(f"\nLength: {len(chunk.page_content)}")
+    print(f"Metadata: {chunk.metadata}")
     print("-" * 80)
